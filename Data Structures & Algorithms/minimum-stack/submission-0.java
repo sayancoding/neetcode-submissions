122885663be9a59cbad13ff561194e222c5cce72
SAYAN MAITY <36039196+sayancoding@users.noreply.github.com>
2026-08-11 12:03:12 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,35 @@
+class MinStack {
+
+    Stack<Integer> mainStack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if (mainStack.isEmpty()) return;
+        
+        // Use equals() to avoid Java Integer caching bugs!
+        if (mainStack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        mainStack.pop();
+    }
+    
+    public int top() {
+        return mainStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
